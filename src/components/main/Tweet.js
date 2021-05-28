b767824033949dbd812b49e7b1095d7567a12ec8
Kishan Khant <38679780+kkishans@users.noreply.github.com>
2021-05-28 21:57:55 +0530

    Middle part design complete

--- src/components/main/Tweet.js
+++ src/components/main/Tweet.js
@@ -1,21 +1,64 @@
 import React from "react";
 
 function Tweet(props) {
+
+
+   let Img = require("../../assets/" + props.data.profile_pic).default
   return (
-    <div class="tweet">
-      <div class="relataed">
-        <span>Web Devlopment</span>
-      </div>
+    <div className="tweet">
       <div className="user-tweet-body">
         <div className="user-icon">
-
+        <img src={Img} />
         </div>
         <div className="tweet-content">
             <div className="tweet-user-info">
-                {props.data.username}
+               <div className="profile_name"><span> {props.data.profile_name}</span></div>
+               {
+                 props.data.check ? 
+                 <div className="verified">
+                   <img src={require("../../assets/verified.svg").default} />
+                 </div>
+                 : <div></div>
+               }
+               <div className="username"> <span>@{props.data.username}</span></div>
+               <div className="time"><span>. {props.data.time}</span></div>
+               
             </div>
-            <div>
-                {props.data.title}
+            <div className="uset-tweet-msg">
+                <p>{props.data.title}</p>
+            </div>
+            <div className="tweet-bottom-icon">
+                <div className="comment">
+                    <div className="comment-icon"><img src={require("../../assets/comment.svg").default}/> </div>
+                    <div> <span> {props.data.comments}</span></div>
+                </div>
+                <div className="retweet">
+                    <div className="retweet-icon"><img src={require("../../assets/retweet.svg").default}/> </div>
+                    <div> <span> {props.data.retweets}</span></div>
+                </div>
+                <div className="like">
+                  {
+                    props.data.liked ?
+                    <>
+                    <div className="like-icon">
+                      <img src={require("../../assets/liked.svg").default}/> 
+                    </div>
+                    <div style={{color:"#D73E69"}}><span> {props.data.likes}</span></div> 
+                    </>
+                    :
+                    <>
+                    <div className="like-icon">
+                      <img src={require("../../assets/like.svg").default}/> 
+                    </div>
+                    <div ><span> {props.data.likes}</span></div> 
+                    </>
+                  }
+                    
+                     
+                </div>
+                <div className="share">
+                    <div className="share-icon"><img src={require("../../assets/upload.svg").default}/> </div>
+                </div>
             </div>
         </div>
       </div>
--- src/data.js
+++ src/data.js
@@ -1,32 +1,49 @@
 const tweet_list = [
   {
     title: "a gemini will like your Tweet and then block you",
-    author: "Twitter",
+    profile_name: "Twitter",
     time: "44m",
     username: "Twitter",
-    retweets: 2200,
-    likes: 12000,
-    comments: 1600,
+    profile_pic:"twitter_profile.jpg",
+    retweets: "2.6k",
+    likes: "1.2k",
+    comments: "1.6k",
+    check:true,
+    liked:false
   },
   {
-    title:
-      "Daylight saving time vs. standard time, which is better for our sleep?",
-    author: "World and Science",
+    title: "Daylight saving time vs. standard time, which is better for our sleep?",
+    profile_name: "World and Science",
     time: "2m",
     username: "WorldAndScience",
+    profile_pic :"WorldAndScience.jpg",
     retweets: 4,
     likes: 10,
     comments: 19,
+    check:false,
+    liked:true
   },
   {
-    title:
-      "Writing a technical article in the simplest possible language is a thing I like.",
-    author: "World and Science",
-    time: "2m",
-    username: "WorldAndScience",
+    title: "Writing a technical article in the simplest possible language is a thing I like.",
+    profile_name: "uncle ief.",
+    time: "2m15h",
+    username: "iefSoBog",
+    profile_pic :"uncle.jpg",
     retweets: 4,
     likes: 10,
     comments: 19,
+    check:false,
+    liked:false
+  },
+  {
+    title:"Late is better than never Slow walking is better than stopping",
+    profile_name: "Pratham",
+    time: "1h",
+    username: "Prathkum",
+    profile_pic :"pratham.jpg",
+    retweets: 4,
+    likes: 12,
+    comments: 164,
   },
 ];
 
