b94de0a571229d907deed39e45849033f3d17b79
Nikunj Thakor <38680557+nnikunjt@users.noreply.github.com>
2021-06-01 18:47:44 +0530

    floating tweet button now hidden from md devices

--- src/components/sidebar/Link.js
+++ src/components/sidebar/Link.js
@@ -1,11 +1,11 @@
 function Link(props) {
     return(
-        <div className={'nav-link ' + (props.name == null ? '' : 'hover-effect')+" "+(props.name=="Search" ? "Searchbtn" : "") }>
+        <div className={'nav-link ' + (props.name === null ? '' : 'hover-effect')+" "+(props.name==="Search" ? "Searchbtn" : "") }>
           {/* <a href="#"> */}
-          <div className={"logo " + (props.name != 'Home' ? '' : 'active')}>
+          <div className={"logo " + (props.name !== 'Home' ? '' : 'active')}>
             {props.logo}
           </div>
-          <div className={'link ' + (props.name != 'Home' ? '' : 'active')}>{props.name}</div>
+          <div className={'link ' + (props.name !== 'Home' ? '' : 'active')}>{props.name}</div>
           {/* </a> */}
         </div>
     )
