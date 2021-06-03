be78984bdda954b3b58147637bb2906951631d7d
Nikunj Thakor <38680557+nnikunjt@users.noreply.github.com>
2021-06-03 19:57:28 +0530

    Update Link.js

--- src/components/sidebar/Link.js
+++ src/components/sidebar/Link.js
@@ -6,8 +6,8 @@ function Link(props) {
         <div className={'nav-link ' + (props.name === null ? '' : 'hover-effect')+" "+(props.name==="Search" ? "Searchbtn" : "") }>
           {/* <a href="#"> */}
           <a href={"/" + props.name} className={"logo " + 
-          (props.name == 'Home' || props.name == 'Search'  ? 'active' : ' deactive')}>
-            {props.logo}
+          ( (((window.location.pathname === '/Home' ||window.location.pathname === '/') && props.name ==="Home" ) || (window.location.pathname === '/Search' && props.name ==="Search"))  ? 'active' : '')}>
+            {props.logo} 
           </a>
           <div className={'link ' + (props.name != 'Home' ? '' : 'active')}>{props.name}</div>
           {/* </a> */}
